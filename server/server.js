a328c586bfa6ae8c173b9e2c0777dd4fc59d75dd
cspranam <cspranam@gmail.com>
2017-11-01 09:59:51 +0000

    add connect and discconnet

--- server/server.js
+++ server/server.js
@@ -1,12 +1,27 @@
 const path = require('path');
+const http = require('http');
 const express = require('express');
+const socketIO = require('socket.io');
 
 const publicPath = path.join(__dirname, "../public");
 const port = 3000 || process.env.PORT;
 
 var app = express();
+var server = http.createServer(app);
+var io = socketIO(server);
+
 app.use(express.static(publicPath));
 
-app.listen(port, () => {
+io.on('connection', (socket) =>{
+	console.log('New user connected');
+	
+	socket.on('disconnect', () => {
+		console.log('User disconnected');
+	});
+});
+
+
+
+server.listen(port, () => {
 	console.log("Server is listening on "+port);
 });
