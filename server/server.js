e647152655bcd0511793643f844abe37687fa9c5
cspranam <cspranam@gmail.com>
2017-11-01 10:35:02 +0000

    add new msg and create msg events

--- server/server.js
+++ server/server.js
@@ -15,6 +15,16 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) =>{
 	console.log('New user connected');
 	
+	socket.emit('newMessage',{
+		from:'mike@ex.com',
+		text:'Hello',
+		createdAt: new Date()
+	}); 
+	
+	socket.on('createMessage', (msg) =>{
+		console.log('New message from client : ', msg);
+	});
+	
 	socket.on('disconnect', () => {
 		console.log('User disconnected');
 	});
