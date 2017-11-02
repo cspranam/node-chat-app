1c7b686f68fff4ef52605ddf7962c0c74f918507
cspranam <cspranam@gmail.com>
2017-11-02 14:58:30 +0000

    chat with basic ui

--- public/js/index.js
+++ public/js/index.js
@@ -9,6 +9,17 @@ socket.on('disconnect', function() {
 
 socket.on('newMessage', function(msg) {
 	console.log('newMessage from server : ', msg);
-	
+	var li = jQuery('<li></li>');
+	li.text(`${msg.from}:${msg.text}`);
+	jQuery(`#messages`).append(li);
 });
 
+jQuery('#message-form').on('submit', function(e){
+	e.preventDefault();
+	socket.emit('createMessage', {
+		from: 'User',
+		text: jQuery('[name=message]').val()
+	}, function(){
+		
+	});
+});
