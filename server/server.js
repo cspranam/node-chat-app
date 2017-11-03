9c850a5cbca435c3b21345f56865a31a9bcad00a
cspranam <cspranam@gmail.com>
2017-11-03 15:27:42 +0000

    css for chat

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ const http = require('http');
 const express = require('express');
 const socketIO = require('socket.io');
 const request = require('request');
-const {generateMessage} = require('./utils/message.js');
+const {generateMessage, generateLocationMessage} = require('./utils/message.js');
 
 const publicPath = path.join(__dirname, "../public");
 const port = process.env.PORT || 3000;
@@ -23,13 +23,11 @@ io.on('connection', (socket) =>{
 	socket.on('createMessage', (msg, callback) =>{
 		console.log('New message from client : ', msg);
 		io.emit('newMessage', generateMessage(msg.from, msg.text));
-		callback('This is from the server');
-		
-		// socket.broadcast.emit('newMessage', {
-			// from:msg.from,
-			// text:msg.text,
-			// createdAt: new Date().getTime()
-		// });
+		callback();
+	});
+	
+	socket.on('createLocationMessage', (coords) => {
+		io.emit('newLocationMessage', generateLocationMessage('Admin', coords.latitude,coords.longitude));
 	});
 	
 	socket.on('disconnect', () => {
