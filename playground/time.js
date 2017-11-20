dd094e716cf5fea599730e4a8a6f993acc5232d8
cspranam <cspranam@gmail.com>
2017-11-20 09:32:18 +0000

    chat app

---
+++ playground/time.js
@@ -0,0 +1,11 @@
+var moment = require('moment');
+
+// var date = new Date().getTime();
+// var months = [];
+// console.log(date.getMonth());
+
+new Date().getTime()
+var someTimestamp = moment().valueOf();
+console.log(someTimestamp);
+var date = moment(createdAt);
+console.log(date.format('h:mm a'));
--- public/js/index.js
+++ public/js/index.js
@@ -9,15 +9,17 @@ socket.on('disconnect', function() {
 
 socket.on('newMessage', function(msg) {
 	console.log('newMessage from server : ', msg);
+	var formattedTime = moment(msg.createdAt).format('h:mm a');
 	var li = jQuery('<li></li>');
-	li.text(`${msg.from}:${msg.text}`);
+	li.text(`${msg.from}:${formattedTime}:${msg.text}`);
 	jQuery(`#messages`).append(li);
 });
 
 socket.on('newLocationMessage', function(msg){
 	var li = jQuery('<li></li>');
 	var a = jQuery('<a target="_blank">My current location</a>');
-	li.text(`${msg.from}:`);
+	var formattedTime = moment(msg.createdAt).format('h:mm a');
+	li.text(`${msg.from}:${formattedTime}:`);
 	a.attr('href',msg.url);
 	li.append(a);
 	jQuery(`#messages`).append(li);
